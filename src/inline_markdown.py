9dbca1dd71fae3055e083d505e9dd6698fdca104
HamishCallow <hamish.mcdermott.yt@gmail.com>
2025-11-26 17:09:00 +1100

    added link extraction for images and links from markdown using regex and added test script

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,5 +1,6 @@
 from textnode import TextNode, TextType
-
+import re
+    
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for old_node in old_nodes:
@@ -15,3 +16,9 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 else:
                     new_nodes.append(TextNode(nodes[i], TextType.TEXT))
     return new_nodes
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
